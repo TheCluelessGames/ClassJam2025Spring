788191a5ffc868c97a90fb68cad9aa58ae77e396
eneyl001 <eneyl001@edu.xamk.fi>
2025-03-30 21:13:06 +0300

    SoundManager added and tested
    SoundManager in mainmenu
    tested with Team and X buttons

--- Assets/Scripts/TeamUIElement.cs
+++ Assets/Scripts/TeamUIElement.cs
@@ -6,16 +6,27 @@ public class TeamUIElement : MonoBehaviour
 {
     [SerializeField] GameObject teamName;
     [SerializeField] GameObject closeButton;
+    [SerializeField] private AudioClip uiButtonSound;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void ShowTeam()
     { 
         teamName.SetActive(true);
         closeButton.SetActive(true);
+        SoundFXManager.Instance.PlaySoundFXClip(uiButtonSound, transform, 0.5f);
+
     }
 
     public void HideTeam()
     {
         teamName.SetActive(false);
         closeButton.SetActive(false);
+        SoundFXManager.Instance.PlaySoundFXClip(uiButtonSound, transform, 0.5f);
     }
 }
