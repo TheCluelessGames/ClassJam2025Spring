e86f72f81f08ee04ab9df5670d3b7d337bbd4770
Stevevokhe <63345005+Stevevokhe@users.noreply.github.com>
2025-03-30 21:35:51 +0300

    Update EnemyCowController.cs

--- Assets/Scripts/EnemyCowController.cs
+++ Assets/Scripts/EnemyCowController.cs
@@ -6,11 +6,18 @@ using static UnityEngine.ParticleSystem;
 
 public class EnemyCowController : MonoBehaviour
 {
+    [SerializeField] private AudioClip sound;
+    private AudioSource audioSource;
     public enum ObjectType {Orange, Purple, Green, White, Red, Blue, Yellow}
     public ObjectType selectedType;
     public Sprite NakedCowHeadSprite, NakedCowBodySprite;
     public SpriteRenderer headRenderer, bodyRenderer;
     public GameObject particlePrefab;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void Undress()
     {
@@ -22,6 +29,7 @@ public class EnemyCowController : MonoBehaviour
     public void Poof()
     {
         GameObject particle = Instantiate(particlePrefab, transform.position, Quaternion.identity);
+        SoundFXManager.Instance.PlaySoundFXClip(sound, transform, 0.5f);
         Destroy(particle, particle.GetComponent<ParticleSystem>().main.duration);
     }
 }
