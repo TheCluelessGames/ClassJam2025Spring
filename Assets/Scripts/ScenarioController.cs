083069ad9b6e1bb26d0b198503e2a9c504c493e2
Stevevokhe <63345005+Stevevokhe@users.noreply.github.com>
2025-03-30 19:22:20 +0300

    Added ScenarioController

---
+++ Assets/Scripts/ScenarioController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioController : MonoBehaviour
+{
+    public EnemyCowController.ObjectType type;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
